47bd8ebd88e48e15dbdfa55ef114789d81f8fa99
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-12-09 08:34:21 +0000

    Script that tests parallel execution

--- tests/testthat/test-zzz-script.R
+++ tests/testthat/test-zzz-script.R
@@ -41,3 +41,30 @@ test_that("error", {
   expect_is(r1, "context_task_error")
   expect_equal(r1$trace[[1]], "context:::main_task_run()")
 })
+
+test_that("parallel", {
+  path <- tempfile("cluster_")
+  on.exit(cleanup(path))
+  ctx <- context_save(path, sources = "myfuns-parallel.R")
+  t <- task_save(quote(get_cluster_pids()), ctx)
+
+  full <- file.path(path_bin(path), "task_run")
+  log1 <- suppressWarnings(
+    Rscript(c(full, path, t), stdout = TRUE, stderr = TRUE))
+  expect_equal(task_status(t, ctx), TASK_ERROR)
+
+  res1 <- task_result(t, ctx)
+  expect_is(res1, "context_task_error")
+  expect_match(res1$message, "registered")
+  expect_true(any(grepl("defaultCluster", res1$trace)))
+
+  Sys.setenv("CONTEXT_CORES" = 2L)
+  on.exit(Sys.unsetenv("CONTEXT_CORES"), add = TRUE)
+  log2 <- Rscript(c(full, path, t), stdout = TRUE, stderr = TRUE)
+
+  expect_equal(task_status(t, ctx), TASK_COMPLETE)
+  res2 <- task_result(t, ctx)
+  expect_true(process_id() != res2$host)
+  expect_equal(length(res2$workers), 2)
+  expect_false(res2$host %in% res2$workers)
+})
