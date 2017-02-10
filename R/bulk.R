6f7c1d4bee0ad360c707e0609351945ec0ed9a1a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-02-10 09:41:03 +0000

    Add bulk task saving infrastructure

---
+++ R/bulk.R
@@ -0,0 +1,106 @@
+task_bulk_prepare <- function(X, FUN, DOTS, do.call, use_names,
+                              envir, db) {
+  XX <- task_bulk_prepare_X(X, do.call, use_names)
+  task_bulk_prepare_expr(XX, FUN, DOTS, do.call)
+}
+
+## This is the new replacement bulk uploader.  It exists to support
+## queuer only really, though will need to be exported (and that will
+## be a bit weird).
+task_bulk_save <- function(x, context) {
+  db <- context$db
+  context_id <- context$id
+
+  build_task <- function(x) {
+    x$id <- ids::random_id()
+    x$context_id <- context_id
+    class(x) <- "task"
+    x
+  }
+
+  n <- length(x)
+  context_log("bulk", sprintf("Creating %s tasks", n))
+  tasks <- lapply(x, build_task)
+  ids <- vcapply(tasks, "[[", "id")
+  ns <- c("tasks", "task_status", "task_context", "task_time_sub")
+  send <- c(tasks,
+            rep(list(TASK_PENDING), n),
+            rep(list(context$id), n),
+            rep(list(Sys.time()), n))
+  db$mset(rep(ids, length(ns)), send, rep(ns, each = n))
+  ids
+}
+
+task_bulk_prepare_X <- function(X, do.call, use_names) {
+  if (is.data.frame(X)) {
+    if (ncol(X) == 0L) {
+      stop("'X' must have at least one column")
+    }
+    if (nrow(X) == 0L) {
+      stop("'X' must have at least one row")
+    }
+    X <- df_to_list(X, use_names || !do.call)
+  } else if (is.atomic(X) && !is.null(X)) {
+    X <- as.list(unname(X))
+  } else if (is.list(X)) {
+    if (do.call) {
+      lens <- lengths(X)
+      if (length(unique(lens)) != 1L) {
+        stop("Every element of 'X' must have the same length")
+      }
+      if (lens[[1L]] == 0L) {
+        stop("Elements of 'X' must have at least one element")
+      }
+      nms <- names(X[[1L]])
+      if (!all(vlapply(X[-1], function(x) identical(names(x), nms)))) {
+        stop("Elements of 'X' must have the same names")
+      }
+      ## This would be useful in the case of moving the ifelse from
+      ## the rewrite function below.
+      ##
+      ## if (lens[[1]] == 0L && !do.call) {
+      ##   X <- lapply(X, list)
+      ## }
+    }
+  } else {
+    stop("X must be a data.frame or list")
+  }
+
+  if (length(X) == 0L) {
+    stop("'X' must have at least one element")
+  }
+
+  X
+}
+
+task_bulk_prepare_expr <- function(X, FUN, DOTS, do.call) {
+  if (do.call) {
+    ## These assumptions about the first element are tested above
+    len <- length(X[[1L]])
+    nms <- names(X[[1L]])
+    template <- as.call(c(list(FUN), setNames(rep(list(NULL), len), nms), DOTS))
+    idx <- seq_len(len) + 1L
+  } else {
+    template <- as.call(c(list(FUN), list(NULL), DOTS))
+    idx <- 2L
+  }
+  template <- context::prepare_expression(template, envir, db)
+
+  ## TODO: I'm not 100% sure that this is a great idea; by running the
+  ## object itself into the call we hit trouble if the objects that
+  ## are being iterated over are nontrivial in size.  Consider
+  ## detecting this and dumping them into the locals if they're big.
+  rewrite_expr <- function(x) {
+    if (do.call) {
+      template$expr[idx] <- x
+      if (!is.null(names(x))) {
+        names(template$expr[idx]) <- names(x)
+      }
+    } else {
+      template$expr[[idx]] <- x
+    }
+    template
+  }
+
+  lapply(X, rewrite_expr)
+}
--- tests/testthat/test-tasks-bulk.R
+++ tests/testthat/test-tasks-bulk.R
@@ -1,5 +1,88 @@
 context("tasks - bulk")
 
+test_that("prepare; single arg", {
+  x <- 1:4
+  cmp <- lapply(x, function(el) list(expr = bquote(foo(.(el)))))
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, TRUE, FALSE), cmp)
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, FALSE, FALSE), cmp)
+})
+
+test_that("prepare; named single arg", {
+  x <- setNames(1:4, letters[1:4])
+  cmp <- lapply(unname(x), function(el) list(expr = bquote(foo(.(el)))))
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, TRUE, FALSE), cmp)
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, FALSE, FALSE), cmp)
+})
+
+test_that("prepare", {
+  x <- list(c(1, 2), c(3, 4))
+
+  ## Not do.call
+  cmp <- lapply(unname(x), function(el) list(expr = bquote(foo(.(el)))))
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, FALSE, FALSE), cmp)
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, FALSE, TRUE), cmp)
+
+  ## do.call
+  cmp <- lapply(x, function(el) list(expr = as.call(c(list(quote(foo)), el))))
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, TRUE, FALSE), cmp)
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, TRUE, TRUE), cmp)
+})
+
+test_that("prepare; data.frame", {
+  df <- data.frame(a = 1:5, b = runif(5))
+
+  ## not do.call, preserving and dropping names:
+  cmp <- lapply(df_to_list(df, TRUE), function(el)
+    list(expr = bquote(foo(.(el)))))
+  expect_equal(task_bulk_prepare(df, quote(foo), NULL, FALSE, TRUE),
+               cmp)
+  expect_equal(task_bulk_prepare(df, quote(foo), NULL, FALSE, FALSE), cmp)
+
+  ## do.call, preserving names
+  cmp <- lapply(df_to_list(df, TRUE), function(el)
+    list(expr = as.call(c(list(quote(foo)), el))))
+  expect_equal(task_bulk_prepare(df, quote(foo), NULL, TRUE, TRUE), cmp)
+
+  ## do.call, dropping names
+  cmp <- lapply(df_to_list(df, TRUE), function(el)
+    list(expr = as.call(c(list(quote(foo)), unname(el)))))
+  expect_equal(task_bulk_prepare(df, quote(foo), NULL, TRUE, FALSE), cmp)
+})
+
+test_that("prepare; uneven length lists", {
+  x <- list(1, c(1, 2))
+  cmp <- lapply(x, function(el) list(expr = bquote(foo(.(el)))))
+  expect_equal(task_bulk_prepare(x, quote(foo), NULL, FALSE, TRUE),
+               cmp)
+
+  ## Error case
+  expect_error(task_bulk_prepare(list(1, 1:2), quote(foo), NULL, TRUE, TRUE),
+               "Every element of 'X' must have the same length")
+})
+
+test_that("prepare; error cases", {
+  expect_error(task_bulk_prepare(NULL, quote(foo), NULL, TRUE, TRUE),
+               "X must be a data.frame or list", fixed = TRUE)
+  expect_error(task_bulk_prepare(quote(sin), quote(foo), NULL, TRUE, TRUE),
+               "X must be a data.frame or list", fixed = TRUE)
+  expect_error(task_bulk_prepare(quote(1 + 2), quote(foo), NULL, TRUE, TRUE),
+               "X must be a data.frame or list", fixed = TRUE)
+
+  ## Zero length
+  expect_error(
+    task_bulk_prepare(df[integer(0), ], quote(foo), NULL, FALSE, TRUE),
+    "'X' must have at least one row", fixed = TRUE)
+  expect_error(
+    task_bulk_prepare(df[, integer(0)], quote(foo), NULL, FALSE, TRUE),
+    "'X' must have at least one column", fixed = TRUE)
+  expect_error(
+    task_bulk_prepare(list(), quote(foo), NULL, FALSE, TRUE),
+    "'X' must have at least one element", fixed = TRUE)
+  expect_error(
+    task_bulk_prepare(list(list(), list()), quote(foo), NULL, TRUE, TRUE),
+    "Elements of 'X' must have at least one element", fixed = TRUE)
+})
+
 ## This is the simplest case; where we only have a single element to
 ## add.
 test_that("simple", {
