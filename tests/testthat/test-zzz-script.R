6484735b9061caf573c8c14db85e2615323def6b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-02-10 07:41:49 +0000

    Temporary directory needs normalising on osx

--- tests/testthat/test-zzz-script.R
+++ tests/testthat/test-zzz-script.R
@@ -94,12 +94,13 @@ test_that("manual parallel cluster", {
   ## of the paths here.  So we test that _any_ of the library paths
   ## are consistent with the library path having been set when we
   ## really should test for the first one.
-  expect_false(any(string_starts_with(paths1[[1]], path)))
-  expect_true(any(string_starts_with(paths2[[1]], path)))
+  path_norm <- normalizePath(path)
+  expect_false(any(string_starts_with(paths1[[1]], path_norm)))
+  expect_true(any(string_starts_with(paths2[[1]], path_norm)))
 
   if (!nzchar(Sys.getenv("R_LIBS"))) {
-    expect_false(string_starts_with(paths1[[1]][[1]], path))
-    expect_true(string_starts_with(paths2[[1]][[1]], path))
+    expect_false(string_starts_with(paths1[[1]][[1]], path_norm))
+    expect_true(string_starts_with(paths2[[1]][[1]], path_norm))
   }
 })
 
